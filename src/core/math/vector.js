8de7dafe708b64e1701729054581d64cabe4ec5b
David Thevenin <david.thevenin@gmail.com>
2014-06-04 14:05:15 +0900

    Split glMatrix into files

---
+++ src/core/math/vector.js
@@ -0,0 +1,413 @@
+/* 
+ * glMatrix.js - High performance matrix and vector operations for WebGL
+ * version 0.9.6
+ */
+ 
+/*
+ * Copyright (c) 2011 Brandon Jones
+ *
+ * This software is provided 'as-is', without any express or implied
+ * warranty. In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ *    1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would be
+ *    appreciated but is not required.
+ *
+ *    2. Altered source versions must be plainly marked as such, and must not
+ *    be misrepresented as being the original software.
+ *
+ *    3. This notice may not be removed or altered from any source
+ *    distribution.
+ */
+
+// Fallback for systems that don't support WebGL
+if(typeof Float32Array != 'undefined') {
+	glMatrixArrayType = Float32Array;
+} else if(typeof WebGLFloatArray != 'undefined') {
+	glMatrixArrayType = WebGLFloatArray; // This is officially deprecated and should dissapear in future revisions.
+} else {
+	glMatrixArrayType = Array;
+}
+
+
+/*
+ * vec2 - 2 Dimensional Vector
+ */
+var vec2 = {};
+
+/*
+ * vec2.create
+ * Creates a new instance of a vec2 using the default array type
+ * Any javascript array containing at least 3 numeric elements can serve as a vec2
+ *
+ * Params:
+ * vec - Optional, vec2 containing values to initialize with
+ *
+ * Returns:
+ * New vec2
+ */
+vec2.create = function(vec) {
+	var dest = new glMatrixArrayType(2);
+	
+	if(vec) {
+		dest[0] = vec[0];
+		dest[1] = vec[1];
+	}
+	
+	return dest;
+};
+
+/*
+ * vec2.set
+ * Copies the values of one vec2 to another
+ *
+ * Params:
+ * vec - vec2 containing values to copy
+ * dest - vec2 receiving copied values
+ *
+ * Returns:
+ * dest
+ */
+vec2.set = function(vec, dest) {
+	dest[0] = vec[0];
+	dest[1] = vec[1];
+	
+	return dest;
+};
+
+
+
+/*
+ * vec3 - 3 Dimensional Vector
+ */
+var vec3 = {};
+
+/*
+ * vec3.create
+ * Creates a new instance of a vec3 using the default array type
+ * Any javascript array containing at least 3 numeric elements can serve as a vec3
+ *
+ * Params:
+ * vec - Optional, vec3 containing values to initialize with
+ *
+ * Returns:
+ * New vec3
+ */
+vec3.create = function(vec) {
+	var dest = new glMatrixArrayType(3);
+	
+	if(vec) {
+		dest[0] = vec[0];
+		dest[1] = vec[1];
+		dest[2] = vec[2];
+	}
+	
+	return dest;
+};
+
+/*
+ * vec3.set
+ * Copies the values of one vec3 to another
+ *
+ * Params:
+ * vec - vec3 containing values to copy
+ * dest - vec3 receiving copied values
+ *
+ * Returns:
+ * dest
+ */
+vec3.set = function(vec, dest) {
+	dest[0] = vec[0];
+	dest[1] = vec[1];
+	dest[2] = vec[2];
+	
+	return dest;
+};
+
+/*
+ * vec3.set_p
+ * Copies the values of one vec3 to another
+ *
+ * Params:
+ * a - vec3[0] containing values to copy
+ * b - vec3[1] containing values to copy
+ * c - vec3[2] containing values to copy
+ * dest - vec3 receiving copied values
+ *
+ * Returns:
+ * dest
+ */
+vec3.set_p = function(a, b, c, dest) {
+	dest[0] = a;
+	dest[1] = b;
+	dest[2] = c;
+	
+	return dest;
+};
+
+/*
+ * vec3.add
+ * Performs a vector addition
+ *
+ * Params:
+ * vec - vec3, first operand
+ * vec2 - vec3, second operand
+ * dest - Optional, vec3 receiving operation result. If not specified result is written to vec
+ *
+ * Returns:
+ * dest if specified, vec otherwise
+ */
+vec3.add = function(vec, vec2, dest) {
+	if(!dest || vec == dest) {
+		vec[0] += vec2[0];
+		vec[1] += vec2[1];
+		vec[2] += vec2[2];
+		return vec;
+	}
+	
+	dest[0] = vec[0] + vec2[0];
+	dest[1] = vec[1] + vec2[1];
+	dest[2] = vec[2] + vec2[2];
+	return dest;
+};
+
+/*
+ * vec3.subtract
+ * Performs a vector subtraction
+ *
+ * Params:
+ * vec - vec3, first operand
+ * vec2 - vec3, second operand
+ * dest - Optional, vec3 receiving operation result. If not specified result is written to vec
+ *
+ * Returns:
+ * dest if specified, vec otherwise
+ */
+vec3.subtract = function(vec, vec2, dest) {
+	if(!dest || vec == dest) {
+		vec[0] -= vec2[0];
+		vec[1] -= vec2[1];
+		vec[2] -= vec2[2];
+		return vec;
+	}
+	
+	dest[0] = vec[0] - vec2[0];
+	dest[1] = vec[1] - vec2[1];
+	dest[2] = vec[2] - vec2[2];
+	return dest;
+};
+
+/*
+ * vec3.negate
+ * Negates the components of a vec3
+ *
+ * Params:
+ * vec - vec3 to negate
+ * dest - Optional, vec3 receiving operation result. If not specified result is written to vec
+ *
+ * Returns:
+ * dest if specified, vec otherwise
+ */
+vec3.negate = function(vec, dest) {
+	if(!dest) { dest = vec; }
+	
+	dest[0] = -vec[0];
+	dest[1] = -vec[1];
+	dest[2] = -vec[2];
+	return dest;
+};
+
+/*
+ * vec3.scale
+ * Multiplies the components of a vec3 by a scalar value
+ *
+ * Params:
+ * vec - vec3 to scale
+ * val - Numeric value to scale by
+ * dest - Optional, vec3 receiving operation result. If not specified result is written to vec
+ *
+ * Returns:
+ * dest if specified, vec otherwise
+ */
+vec3.scale = function(vec, val, dest) {
+	if(!dest || vec == dest) {
+		vec[0] *= val;
+		vec[1] *= val;
+		vec[2] *= val;
+		return vec;
+	}
+	
+	dest[0] = vec[0]*val;
+	dest[1] = vec[1]*val;
+	dest[2] = vec[2]*val;
+	return dest;
+};
+
+/*
+ * vec3.normalize
+ * Generates a unit vector of the same direction as the provided vec3
+ * If vector length is 0, returns [0, 0, 0]
+ *
+ * Params:
+ * vec - vec3 to normalize
+ * dest - Optional, vec3 receiving operation result. If not specified result is written to vec
+ *
+ * Returns:
+ * dest if specified, vec otherwise
+ */
+vec3.normalize = function(vec, dest) {
+	if(!dest) { dest = vec; }
+	
+	var x = vec[0], y = vec[1], z = vec[2];
+	var len = Math.sqrt(x*x + y*y + z*z);
+	
+	if (!len) {
+		dest[0] = 0;
+		dest[1] = 0;
+		dest[2] = 0;
+		return dest;
+	} else if (len == 1) {
+		dest[0] = x;
+		dest[1] = y;
+		dest[2] = z;
+		return dest;
+	}
+	
+	len = 1 / len;
+	dest[0] = x*len;
+	dest[1] = y*len;
+	dest[2] = z*len;
+	return dest;
+};
+
+/*
+ * vec3.cross
+ * Generates the cross product of two vec3s
+ *
+ * Params:
+ * vec - vec3, first operand
+ * vec2 - vec3, second operand
+ * dest - Optional, vec3 receiving operation result. If not specified result is written to vec
+ *
+ * Returns:
+ * dest if specified, vec otherwise
+ */
+vec3.cross = function(vec, vec2, dest){
+	if(!dest) { dest = vec; }
+	
+	var x = vec[0], y = vec[1], z = vec[2];
+	var x2 = vec2[0], y2 = vec2[1], z2 = vec2[2];
+	
+	dest[0] = y*z2 - z*y2;
+	dest[1] = z*x2 - x*z2;
+	dest[2] = x*y2 - y*x2;
+	return dest;
+};
+
+/*
+ * vec3.length
+ * Caclulates the length of a vec3
+ *
+ * Params:
+ * vec - vec3 to calculate length of
+ *
+ * Returns:
+ * Length of vec
+ */
+vec3.length = function(vec){
+	var x = vec[0], y = vec[1], z = vec[2];
+	return Math.sqrt(x*x + y*y + z*z);
+};
+
+/*
+ * vec3.dot
+ * Caclulates the dot product of two vec3s
+ *
+ * Params:
+ * vec - vec3, first operand
+ * vec2 - vec3, second operand
+ *
+ * Returns:
+ * Dot product of vec and vec2
+ */
+vec3.dot = function(vec, vec2){
+	return vec[0]*vec2[0] + vec[1]*vec2[1] + vec[2]*vec2[2];
+};
+
+/*
+ * vec3.direction
+ * Generates a unit vector pointing from one vector to another
+ *
+ * Params:
+ * vec - origin vec3
+ * vec2 - vec3 to point to
+ * dest - Optional, vec3 receiving operation result. If not specified result is written to vec
+ *
+ * Returns:
+ * dest if specified, vec otherwise
+ */
+vec3.direction = function(vec, vec2, dest) {
+	if(!dest) { dest = vec; }
+	
+	var x = vec[0] - vec2[0];
+	var y = vec[1] - vec2[1];
+	var z = vec[2] - vec2[2];
+	
+	var len = Math.sqrt(x*x + y*y + z*z);
+	if (!len) { 
+		dest[0] = 0; 
+		dest[1] = 0; 
+		dest[2] = 0;
+		return dest; 
+	}
+	
+	len = 1 / len;
+	dest[0] = x * len; 
+	dest[1] = y * len; 
+	dest[2] = z * len;
+	return dest; 
+};
+
+/*
+ * vec3.lerp
+ * Performs a linear interpolation between two vec3
+ *
+ * Params:
+ * vec - vec3, first vector
+ * vec2 - vec3, second vector
+ * lerp - interpolation amount between the two inputs
+ * dest - Optional, vec3 receiving operation result. If not specified result is written to vec
+ *
+ * Returns:
+ * dest if specified, vec otherwise
+ */
+vec3.lerp = function(vec, vec2, lerp, dest){
+    if(!dest) { dest = vec; }
+    
+    dest[0] = vec[0] + lerp * (vec2[0] - vec[0]);
+    dest[1] = vec[1] + lerp * (vec2[1] - vec[1]);
+    dest[2] = vec[2] + lerp * (vec2[2] - vec[2]);
+    
+    return dest;
+}
+
+/*
+ * vec3.str
+ * Returns a string representation of a vector
+ *
+ * Params:
+ * vec - vec3 to represent as a string
+ *
+ * Returns:
+ * string representation of vec
+ */
+vec3.str = function(vec) {
+	return '[' + vec[0] + ', ' + vec[1] + ', ' + vec[2] + ']'; 
+};
+
