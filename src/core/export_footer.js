db083e2ee6bc592d0989851e38ceb6f3969a9deb
David Thevenin <david.thevenin@gmail.com>
2014-07-03 15:50:04 +0900

    Add export file system

---
+++ src/core/export_footer.js
@@ -0,0 +1,33 @@
+/********************************************************************
+                      Export
+*********************************************************************/
+return {
+  POINTER_START: POINTER_START,
+  POINTER_MOVE: POINTER_MOVE,
+  POINTER_END: POINTER_END,
+  POINTER_CANCEL: POINTER_CANCEL,
+//  EVENT_SUPPORT_GESTURE,
+//  GESTURE_START,
+//  GESTURE_CHANGE,
+//  GESTURE_END,
+  scheduleAction: scheduleAction,
+  ON_NEXT_FRAME: ON_NEXT_FRAME,
+  Style: Style,
+  Color: Color,
+  Event: Event,
+  Group: Group,
+  Transform: Transform,
+  Canvas: Canvas,
+  Image: gl_Image,
+  Text: Text,
+  View: View,
+  Application: Application,
+  createClass: createClass
+};
+
+//gl.createCustomEvent = createCustomEvent;
+//gl.removePointerListener = removePointerListener;
+//gl.addPointerListener = addPointerListener;
+//gl.PointerTypes = PointerTypes;
+
+});
