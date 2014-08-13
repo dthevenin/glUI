dae84b936dc598bb4768a24d8452b60e57bce53e
David Thevenin <david.thevenin@gmail.com>
2014-08-13 14:20:38 +0900

    Add profile collect clean and console print

--- src/profiling/profiling.js
+++ src/profiling/profiling.js
@@ -8,8 +8,12 @@ var
 profiling.setCollectProfile = function (value) {
   if (!value) {
     profiling.collect = false;
+    printProfilingData ();
+    console.log ("End prolile collect.");
   }
   else {
+    console.log ("Start prolile collect.");
+    cleanProfilingData ();
     profiling.collect = true;
   }
 }
@@ -68,6 +72,36 @@ function getProfilingProbeId (name, info) {
   };
 
   return _profiling_id++;
+}
+
+function cleanProfilingData () {
+  _profiling_data.forEach (function (data) {
+    data.measures.length = 0;
+  })
+}
+
+function printProfilingData () {
+  _profiling_data.forEach (function (data) {
+    
+    var nb_mesure = 0, total = 0, temp = 0;
+    
+    data.measures.forEach (function (measure) {
+      if (measure[0] === 0) {
+        // bigin
+        temp = measure[1];
+      }
+      if (measure[0] === 1 && temp) {
+        total += (measure[1] - temp);
+        temp = 0;
+        nb_mesure ++;
+      }
+    })
+    
+    console.log (
+      "Measure '" + data.name + "' [" + nb_mesure + "]:  " +
+      (total / nb_mesure) + "ms"
+    )
+  })
 }
 
 profiling.begin = start_profiling_data;
